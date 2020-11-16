806784da470b4e27410c751054e5860fe3ef8c0e
RoTour <rothewawa@gmail.com>
2020-11-16 18:17:46 +0100

    database reset/new migration files

--- db/migrate/20201116171612_create_articles.rb
+++ db/migrate/20201116171612_create_articles.rb
@@ -1,6 +1,6 @@
-class CreateArticles2 < ActiveRecord::Migration[6.0]
+class CreateArticles < ActiveRecord::Migration[6.0]
   def change
-    create_table :articles2s do |t|
+    create_table :articles do |t|
       t.string :title
       t.text :description
       t.datetime :uploaded_at
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_162632) do
+ActiveRecord::Schema.define(version: 2020_11_16_171612) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "created_at"
+    t.datetime "uploaded_at"
     t.datetime "updated_at"
   end
 

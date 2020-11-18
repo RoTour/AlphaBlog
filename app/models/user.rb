fd2d605828a22452c144934cdf9fc9de32e83370
RoTour <rothewawa@gmail.com>
2020-11-18 10:12:14 +0100

    Add password feature to the back end

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
+
+  before_save { self.email = email.downcase }
+
   has_many :articles
+
   validates :username,
             presence: true,
             uniqueness: { case_sensitive: false },
@@ -8,10 +12,12 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   # Should work but doesn't
   # VALID_EMAIL_REGEX = URI::MailTo::REGEXP
-
   validates :email,
             presence: true,
             uniqueness: { case_sensitive: false },
             length: {maximum: 105},
             format: { with: VALID_EMAIL_REGEX }
+
+  has_secure_password
+
 end

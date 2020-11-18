67dc31c964da95b7f50fc1e9f63cf787ff299106
RoTour <rothewawa@gmail.com>
2020-11-18 11:37:58 +0100

    Add user profile (show)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
   def new
     @user = User.new
   end
@@ -32,4 +38,6 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+
 end
+
